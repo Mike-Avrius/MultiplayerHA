488535ae1b18370dcae90e54cc41197c6c19df44
Mike-Avrius <mike.nidam@gmail.com>
2026-05-19 13:10:45 +0300

    Leave lobby method + Number of players

--- UnityMultiplayer_Tiltan/Assets/Scripts/LobbyManager.cs
+++ UnityMultiplayer_Tiltan/Assets/Scripts/LobbyManager.cs
@@ -7,14 +7,23 @@ using TMPro;
 
 public class LobbyManager : MonoBehaviour, INetworkRunnerCallbacks
 {
-    [SerializeField] private TMP_InputField lobbyNameInput;
-    [SerializeField] NetworkRunner networkRunner;
-    [SerializeField] private GameObject sessionPanel;
-    [SerializeField] private GameObject lobbyPanel;
-
     
+    [SerializeField] NetworkRunner networkRunner;
+    
+    [Header("Lobby")]
+    [SerializeField] private GameObject lobbyPanel;
+    [SerializeField] private TMP_InputField lobbyNameInput;
+    
+    [Header("Session")]
+    [SerializeField] private GameObject sessionPanel;
     [SerializeField] private Transform contentParent;
     [SerializeField] private GameObject roomButtonPrefab;
+    
+    [Header("Room")]
+    [SerializeField] private GameObject roomPanel;
+    [SerializeField] private TextMeshProUGUI numberOfPlayers;
+   
+    
 
     private void Awake()
     {
@@ -37,6 +46,17 @@ public class LobbyManager : MonoBehaviour, INetworkRunnerCallbacks
             
     }
 
+    public async void LeaveLobby()
+    {
+        if (networkRunner.IsRunning)
+        {
+            await networkRunner.Shutdown();
+        }
+        
+        sessionPanel.SetActive(false);
+        lobbyPanel.SetActive(true);
+    }
+
     public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
     {
         foreach (var session in sessionList)
@@ -47,95 +67,114 @@ public class LobbyManager : MonoBehaviour, INetworkRunnerCallbacks
         }
     }
     
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        bool isLocalPLayer = networkRunner.LocalPlayer == player;
+        
+        Debug.Log($"Player {player.PlayerId} joined, local player: {isLocalPLayer}");
+        
+        RefreshUI_Room();
+    }
+    
+    
+    void RefreshUI_Room()
+    {
+#if LOBBY_MANAGER_UI
+        if(networkRunner.IsRunning && !networkRunner.IsShutdown)
+        {
+            sessionPanel.SetActive(false);
+            roomPanel.SetActive(true);
+            string playerNum = networkRunner.SessionInfo?.PlayerCount.ToString();
+            numberOfPlayers.text = $"Available players: {playerNum}";
+        }
+#endif   
+    }
     
 
     public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
     {
-        throw new NotImplementedException();
+     //   throw new NotImplementedException();
     }
 
     public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
     {
-        throw new NotImplementedException();
+      //  throw new NotImplementedException();
     }
 
-    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
-    {
-        throw new NotImplementedException();
-    }
+    
 
     public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
     {
-        throw new NotImplementedException();
+      //  throw new NotImplementedException();
     }
 
     public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
     {
-        throw new NotImplementedException();
+       // throw new NotImplementedException();
     }
 
     public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
     {
-        throw new NotImplementedException();
+       // throw new NotImplementedException();
     }
 
     public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
     {
-        throw new NotImplementedException();
+//throw new NotImplementedException();
     }
 
     public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
     {
-        throw new NotImplementedException();
+       // throw new NotImplementedException();
     }
 
     public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
     {
-        throw new NotImplementedException();
+      //  throw new NotImplementedException();
     }
 
     public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
     {
-        throw new NotImplementedException();
+       // throw new NotImplementedException();
     }
 
     public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
     {
-        throw new NotImplementedException();
+      //  throw new NotImplementedException();
     }
 
     public void OnInput(NetworkRunner runner, NetworkInput input)
     {
-        throw new NotImplementedException();
+//        throw new NotImplementedException();
     }
 
     public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
     {
-        throw new NotImplementedException();
+       // throw new NotImplementedException();
     }
 
     public void OnConnectedToServer(NetworkRunner runner)
     {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
     }
     
     public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
     {
-        throw new NotImplementedException();
+       // throw new NotImplementedException();
     }
 
     public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
     {
-        throw new NotImplementedException();
+       // throw new NotImplementedException();
     }
 
     public void OnSceneLoadDone(NetworkRunner runner)
     {
-        throw new NotImplementedException();
+       // throw new NotImplementedException();
     }
 
     public void OnSceneLoadStart(NetworkRunner runner)
     {
-        throw new NotImplementedException();
+       // throw new NotImplementedException();
     }
 }
